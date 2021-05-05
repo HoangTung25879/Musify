bac3bf2c118ae3d6f581c7b80d8b831758382516
DESKTOP-FJAIC4F\HOANG TUNG <hoangtung2579@gmail.com>
2021-05-05 21:06:52 +0700

    dagger-hilt + music service setup

---
+++ app/src/main/java/com/example/musify/depinjection/AppModule.kt
@@ -0,0 +1,28 @@
+package com.example.musify.depinjection
+
+import android.content.Context
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.example.musify.R
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+    @Singleton
+    @Provides
+    fun provideGlideInstance(
+        @ApplicationContext context: Context
+    ) = Glide.with(context).setDefaultRequestOptions(
+        RequestOptions()
+            .placeholder(R.drawable.ic_baseline_error_24)
+            .error(R.drawable.ic_baseline_error_24)
+            .diskCacheStrategy(DiskCacheStrategy.DATA)
+    )
+}
